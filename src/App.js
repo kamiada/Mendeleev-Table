2e2071ef0fc7593a5a86414e40682ba06ed50772
Adrianna Kaminska <adrianna.kaminska@bbc.co.uk>
2020-09-03 10:17:17 +0100

    starting all over again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Table from './components/Table';
+import TableV2 from './components/TableV2';
 
 function App() {
   return (
     <div className="App">
-      <Table/>
+      <TableV2/>
     </div>
   );
 }
---
+++ src/components/TableV2.jsx
@@ -0,0 +1,3 @@
+import React, { Component, Fragment } from 'react';
+import Element from './Element';
+import './tableV2.scss';
