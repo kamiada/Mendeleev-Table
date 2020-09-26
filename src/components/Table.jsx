004e1b8440a5c704554ce5272b1266f509bb59e5
Adrianna Kaminska <adrianna.kaminska@bbc.co.uk>
2020-09-26 15:34:55 +0100

    found a way to reach atomic number

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,7 +3,6 @@ import React, { Component, Fragment } from "react";
 import Element from "./Element";
 import "./table.scss";
 import data from "../elementsData.json";
-import ReactDOM from 'react-dom';
 
 class TableV2 extends Component {
   constructor(props){
@@ -16,8 +15,10 @@ class TableV2 extends Component {
   }
   printArrayOfAtomicNumbs(array){
     console.log(array);
-    for(let n = 0; n< array.length; n++){
-    console.log('yolo', n)}
+    var element = "";
+    for(element in array){
+     console.log(array[element].key);
+    }
   }
 
   generateEmptySquares = (x) => {
@@ -29,7 +30,7 @@ class TableV2 extends Component {
   };
   fillTable = (data) => {
     const firstElement = (
-      <div id={data[0]._id} key={data[0]._id} className="square" onClick={() => this.printOneAtomicNumb(data[0].atomic_number)}>
+      <div id={data[0].atomic_number} key={data[0].atomic_number} className="square" onClick={() => this.printOneAtomicNumb(data[0].atomic_number)}>
         <Element
           atomicNumb={data[0].atomic_number}
           symbol={data[0].symbol}
@@ -60,7 +61,7 @@ class TableV2 extends Component {
     for (var i = 4; i < data.length; i++) {
       if (i < 12) {
         from4to11.push(
-          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square">
+          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square" >
             <Element
               atomicNumb={data[i].atomic_number}
               symbol={data[i].symbol}
@@ -74,7 +75,7 @@ class TableV2 extends Component {
     for (var i = 12; i < data.length; i++) {
       if (i < 90) {
         from12to89.push(
-          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square">
+          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square" >
             <Element
               atomicNumb={data[i].atomic_number}
               symbol={data[i].symbol}
@@ -88,7 +89,7 @@ class TableV2 extends Component {
     for (var i = 90; i < data.length; i++) {
       if (i < 104) {
         from90to103.push(
-          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square">
+          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square" >
             <Element
               atomicNumb={data[i].atomic_number}
               symbol={data[i].symbol}
@@ -102,7 +103,7 @@ class TableV2 extends Component {
     for (var i = 104; i < data.length; i++) {
       if (i < 118) {
         from104to117.push(
-          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square">
+          <div id={data[i].atomic_number} key={data[i].atomic_number} className="square" >
             <Element
               atomicNumb={data[i].atomic_number}
               symbol={data[i].symbol}
