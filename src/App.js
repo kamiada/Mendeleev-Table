cf8e9239bdbd4ec4f26bc243b459477d38bc8aa9
kamiada <kamiada528@gmail.com>
2021-02-18 00:33:46 +0000

    only misses data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Table from './components/Table';
 import TableV2 from './components/TableV2';
-import TableV3 from './components/TableV3';
 
 function App() {
   return (
     <div className="App">
-      <TableV3/>
+      <TableV2/>
     </div>
   );
 }
