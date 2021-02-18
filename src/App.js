a267ab8b2c6889672585b8d257d68cb4c0dd0fd4
kamiada <kamiada528@gmail.com>
2021-02-18 00:56:54 +0000

    table 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Table from './components/Table';
 import TableV2 from './components/TableV2';
+import TableV3 from './components/TableV3';
 
 function App() {
   return (
     <div className="App">
-      <TableV2/>
+      <TableV3/>
     </div>
   );
 }
